1d364e397b010c762bf3ed76ecfb1c4b35fbfcd8
Evan Schneider <evanrs@gmail.com>
2015-08-10 18:22:04 -0400

    Add mock api endpoints.

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-var koa = require('koa');
+var express = require('express');
 
-var app = koa();
+var app = express();
+var db = require('./db');
 
-app.use(function * () {
-    this.body = "Derp doop, darp dlop";
+app.get('/api/users/:userId', function (req, res, next) {
+    db.users.find(req.params.userId, function (err, user) {
+        if (err) {
+            next(err);
+        }
+        else {
+            res.send(user);
+        }
+    })
 });
 
-app.listen(3000);
+app.get('/api/users', function (req, res) {
+    var users = new Promise(function (resolve, reject) {
+        db.users.all(function (err, users) {
+            if (err)
+                reject(err);
+            else
+                resolve(users);
+        });
+    })
+    .then(function (users) {
+        return Promise.all(users.map(function (userId, index) {
+            return new Promise(function (resolve, reject) {
+                db.users.find(userId, function (err, user) {
+                    if (err)
+                        reject(err);
+                    else {
+                        resolve(user);
+                    }
+                })
+            })
+        }));
+    })
+    .then(function (users) {
+        res.send(users);
+    })
+    .catch(function (err) {
+        res.send(err);
+    });
+});
+
+app.listen(3000, function () {
+    console.log("Listening at", 3000);
+});
